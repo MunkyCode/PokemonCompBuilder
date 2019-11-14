fdaf5ed0d58fa958b4ebe3ecb63413b41a21f5d3
Jolie Elins <jolieelins@Jolie-Bettes-MacBook.local>
2019-11-13 21:27:00 -0500

    a thing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 //COMMENT to boost my commit and contributions
 //Commetn 2
+//jolie comment
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout <<"testing COmmit" << std::endl;
