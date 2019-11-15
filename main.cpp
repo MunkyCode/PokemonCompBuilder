6dda8d793fdc4575113dd1d360d236b9faefb462
Eli Robinson <elimrobinson@gmail.com>
2019-11-15 10:51:51 -0500

    Added FileIO and testing fileIO. Removed random commit tests

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <fstream>
 //COMMENT to boost my commit and contributions
-//Commetn 2
-//jolie comment
+
+void printToFile(std::string fileName){
+    std::ofstream outf(fileName);
+    if (outf){
+        outf <<"testing line 1"<< std::endl;
+        outf <<"testing line 2"<< std::endl;
+    }
+    outf.close();
+}
+void printFromFile(std::string fileName){
+std::ifstream infile(fileName);
+    if (infile) {
+        while (infile) {
+            std::string line;
+            getline(infile, line);
+            std::cout<<line<<std::endl;
+        }
+    } else {
+    }
+}
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout <<"testing Commit round 2" << std::endl;
+    printToFile("testFile.txt");
+    printFromFile("testFile.txt");
+
+
+
     return 0;
 }
