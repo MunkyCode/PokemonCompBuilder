f553b2d1d2f71655e98d3f8f7fd6f83b17662923
Jolie Elins <jolieelins@Jolie-Bettes-MacBook.local>
2019-11-18 14:08:51 -0500

    adding to pokemon.cpp

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -3,3 +3,24 @@
 //
 #include "Pokemon.h"
 
+Pokemon::Pokemon(std::string nameIn, int pokedexIn, std::string typeIn) {
+    this->name = nameIn;
+    this->pokedex = pokedexIn;
+    this->type = typeIn;
+}
+
+std::string Pokemon::getName(){
+    return name;
+}
+
+int Pokemon::getPokedex(){
+    return pokedex;
+}
+
+std::string Pokemon::getType(){
+    return type;
+}
+
+std::string Pokemon::toString(){
+    
+}
