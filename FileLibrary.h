c77bbdd69fbb2a3b6582753399b168da43227a78
Stephen <stevoschool@gmail.com>
2019-11-20 17:17:15 -0500

    Comments in FileLibrary.h

--- FileLibrary.h
+++ FileLibrary.h
@@ -7,10 +7,32 @@
 
 #ifndef POKEMONCOMPBUILDER_FILELIBRARY_H
 #define POKEMONCOMPBUILDER_FILELIBRARY_H
-
+/**
+ * Takes list of pokemon and writes it to file
+ * @param fileName Name of file you wish to write to
+ * @param pokeList arrayList of pokemon
+ * @param size
+ * @throws IDK my exeptions but wrong type if not a pokemon list
+ */
 void printToFilePokemon(std::string fileName, ArrayList* pokeList, int size);
+/**
+ *
+ * @param fileName
+ * @param teamList
+ * @param size
+ */
 void printToFileTeam(std::string fileName, ArrayList* teamList int size);
+/**
+ *
+ * @param fileName
+ * @return
+ */
 ArrayList* readFromFilePokemon(std::string fileName);
+/**
+ *
+ * @param fileName
+ * @return
+ */
 ArrayList* readFromFileTeam(std::string fileName;)
 
 
