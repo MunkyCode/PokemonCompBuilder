01adc137e8036b3c6bbac5173ee446e20da7e1ed
Stephen <stevoschool@gmail.com>
2019-12-02 13:13:59 -0500

    changes to typeEffectivness function

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -5,7 +5,7 @@
 #include "TypeEffectiveness.cpp"
 #include "ArrayList.h"
 
-Pokemon::Pokemon(std::string nameIn, int pokedexIn, std::string typeIn, std::string type2In, std::string abilityIn, int generationIn) {
+Pokemon::Pokemon(std::string nameIn, int pokedexIn, std::string typeIn, std::string type2In, std::string abilityIn, int generationIn, std::string eggGroupIn) {
     this->name = nameIn;
     this->pokedex = pokedexIn;
     this->type = typeIn;
@@ -41,11 +41,21 @@ std::string Pokemon::toString(){
 }
 
 //Returns a list of type efficiencies. Each type is corrolated with a number
+// return list must be deleted by calling function.
 // and the efficiences are .25,.5,1,2 and 4 depending on the move.
-float* GetEffectiveTypes(){
-    int* finalArray[18];
-    int* array1 = arrays[types.find(type)];
-    int* array2 = arrays[types.find(type2)];
+float* Pokemon::GetEffectiveTypes(){
+    float* finalArray = new float[18];
+    int array1Index, array2Index;
+    for(int x = 0; x < 19; x++){
+        if(types[x] == this->type){
+            array1Index = x;
+        }
+        if(types[x] == this-> type2){
+            array2Index = x;
+        }
+    }
+    float* array1 = arrays[array1Index];
+    float* array2 = arrays[array2Index];
     for(int i=0;i<18;i++){
         finalArray[i] = array1[i]*array2[i];
     }
