2b9a9ec4347f70e4f81cfde775a144a5231af345
Eli Robinson <elimrobinson@gmail.com>
2019-12-03 11:44:08 -0500

    FileLibrary.cpp functions added

--- FileLibrary.cpp
+++ FileLibrary.cpp
@@ -3,6 +3,9 @@
 //
 #include <iostream>
 #include <fstream>
+#include<vector>
+#include<sstream>
+#include <w32api/wsman.h>
 #include "Pokemon.h"
 #include"Team.h"
 #include "ArrayList.h"
@@ -16,13 +19,49 @@ void printToFile(std::string fileName){
     }
     outf.close();
 }
-void printFromFile(std::string fileName){
+
+ArrayList<Pokemon*>* CreatePokemonList(std::string fileName){
+    ArrayList<Pokemon*>* pokemonDatabase = new ArrayList<Pokemon*>*(801);
     std::ifstream infile(fileName);
+    std::string name;
+    int dex;
+    std::string type1;
+    std::string type2;
+    std::string abilities;
+    int gen;
     if (infile) {
+        std::vector<std::string> result;
+        int count = 0;
         while (infile) {
             std::string line;
+            std::stringstream s_stream(line); //create string stream from the string
+            int count = 0;
+            while (s_stream.good()) {
+                std::string substr;
+                getline(s_stream, substr, ','); //get first string delimited by comma
+                if(count==0){
+                    name = substr;
+                }
+                if(count==1){
+                    dex = std::stoi(substr);
+                }
+                if(count==2){
+                    type1 = substr;
+                }
+                if(count==3){
+                    type2 = substr;
+                }
+                if(count==4){
+                    abilities = substr;
+                }
+                if(count==5){
+                    gen = stoi(substr);
+                }
+                count +=1;
+            }
+            Pokemon* generic = new Pokemon(name,dex,type1,type2,abilities,gen);
             getline(infile, line);
-            std::cout<<line<<std::endl;
+            pokemonDatabase->insertAtEnd(generic);
         }
     } else {
     }
