e88cee9e26f9e812ab638cc2601394ae5d317d94
Stephen <stevoschool@gmail.com>
2019-12-02 19:13:33 -0500

    Progress on tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include "Pokemon.h"
+#include "Team.h"
 #include "ArrayList.h"
+#include "TestLib.h"
 //COMMENT to boost my commit and contributions
 
 void printToFile(std::string fileName){
@@ -23,24 +25,62 @@ std::ifstream infile(fileName);
     } else {
     }
 }
+void pokemonTest(){
+    std::string types[18] = {"normal", "fighting", "flying", "poison", "ground", "rock", "bug", "ghost", "steel", "fire", "water", "grass", "electric", "psychic", "ice", "dragon", "dark", "fairy"};
+    Pokemon* p1 = new Pokemon("Bulbasaur", 1, "grass","poison","['Overgrow', 'Chlorophyll']", 1);
+    Pokemon* p2 = new Pokemon("Squirtle", 7, "water", "", "['Torrent', 'Rain Dish']", 1);
+    Pokemon* p3 = new Pokemon("Charmander", 4, "fire","testing bad type","['Blaze', 'Solar Power']", 1);
+    std::cout<<"-----------Pokemon Test----------"<<std::endl;
+    std::cout<<"Getters testing:"<<std::endl;
+    printAssertEquals("Bulbasaur",p1->getName());
+    printAssertEquals("Squirtle",p2->getName());
+    printAssertEquals("Charmander",p3->getName());
+    printAssertEquals(1, p1->getPokedex());
+    printAssertEquals(7, p2->getPokedex());
+    printAssertEquals("grass", p1->getType());
+    printAssertEquals("",p3->getType2());
 
-//void pokemonFromString(std::string line);
-
-void testPokemonFromFile(std::string fileName){
-    std::ifstream inFile("pokemonData.csv");
-    if(inFile){
-        while(inFile){
-            std::string line;
-            getline(inFile, line);
-            //pokemonFromString(line);
-        }
+    std::cout<<"Type Effectivness Testing:"<<std::endl;
+    std::cout<<"Bulbasuar:"<<std::endl;
+    float* p1Effectivness = p1->getEffectiveTypes();
+    for(int x = 0; x < 18; x++){
+        std::cout<<types[x]<<": "<<p1Effectivness[x]<<", ";
     }
+    std::cout<<"\nSquirtle:\n";
+    float* p2Effectivness = p2->getEffectiveTypes();
+    for(int x = 0; x < 18; x++){
+        std::cout<<types[x]<<": "<<p2Effectivness[x]<<", ";
+    }
+    std::cout<<"\nChamrander:\n";
+    float* p3Effectivness = p3->getEffectiveTypes();
+    for(int x = 0; x < 18; x++){
+        std::cout<<types[x]<<": "<<p3Effectivness[x]<<", ";
+    }
+    std::cout<<std::endl;
+
 }
 
 void testArrayList(){
     std::cout<< "--------ArrayList test---------"<<std::endl;
+    std::cout<<"Basic Tests:"<<std::endl;
     ArrayList<int>* intList = new ArrayList<int>(4);
-    std::cout << intList->isEmpty()<<std::endl;
+
+
+
+    ArrayList<float>* floatList = new ArrayList<float>(4);
+    floatList->insertAtEnd(10);
+    std::cout<<floatList->itemCount()<<std::endl;
+    std::cout<<floatList->removeValueAtEnd()<<std::endl;
+    std::cout<<floatList->itemCount()<<std::endl;
+    delete intList, floatList;
+    Pokemon* p1 = new Pokemon("Bulbasaur", 1, "grass","poison","['Overgrow', 'Chlorophyll']", 1);
+    Pokemon* p2 = new Pokemon("Squirtle", 7, "water", "", "['Torrent', 'Rain Dish']", 1);
+    Pokemon* p3 = new Pokemon("Charmander", 4, "fire","","['Blaze', 'Solar Power']", 1);
+    ArrayList<Pokemon*>* pokeList = new ArrayList<Pokemon*>(4);
+    pokeList->insertAtEnd(p1);
+    std::cout<<pokeList->getValueAt(0)->getName()<<std::endl;
+
+
 }
 
 
@@ -49,6 +89,7 @@ int main() {
     //printToFile("testFile.txt");
     //printFromFile("testFile.txt");
     //testPokemonFromFile("PokemonData.csv");
+    pokemonTest();
     testArrayList();
 
 
