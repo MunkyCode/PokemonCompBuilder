4ba5b7138b8a4204ef6ca85cab0ea8adcb330d0d
Jolie Elins <jolieelins@Jolie-Bettes-MacBook.local>
2019-12-13 14:39:48 -0500

    UI work

--- UserInterface.cpp
+++ UserInterface.cpp
@@ -2,6 +2,113 @@
 // Created by Jolie Elins on 12/11/19.
 //
 
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "Pokemon.h"
+#include "Team.h"
+#include "ArrayList.h"
+#include "TestLib.h"
+#include "PokemonList.h"
+#include "FileLibrary.h"
+#include "SmartBuilder.h"
 
-//while user input is not exit
-//put in includes
+
+int main() {
+    srand(time(NULL));
+    PokemonList* pokeList;
+    pokeList = createPokemonList("Pokemon - Data.csv");
+
+    std::string input = "";
+    int input2 = -1;
+
+
+    std::cout <<" 1: Display all pokemon \n 2: Search for a pokemon \n 3: Display pokemon by type \n"
+                " 4: Display all current teams \n 5: Display type effectiveness of a team \n"
+                " 6: Create a new team \n 7: Edit a team \n 8: Create a balanced team \n 9: Fill a team \n"
+                " 10: Counter a team \n 11: Import teams \n 12: Export teams \n 'help' to repeat menu \n 'quit' if done \n ->";
+
+    while(input2 > 12 || input2 < 1|| input != "quit"||input!="help"){
+        std::getline(std::cin, input);
+        try {
+            input = stoi(input);
+        }
+        catch(std::exception& e){
+            std::cout<<e.what()<<std::endl;
+        }
+    }
+
+
+    while (input != "quit"){
+        if (input == "help"){
+           std::cout << " 1: Display all pokemon \n 2: Search for a pokemon \n 3: Display pokemon by type \n "
+            " 4: Display all current teams \n 5: Display type effectiveness of a team \n"
+            " 6: Create a new team \n 7: Edit a team \n 8: Create a balanced team \n 9: Fill a team \n"
+            "10: Counter a team \n 11: Import teams \n 12: Export teams \n 'help' to repeat menu \n 'quit' if done \n";
+        }
+        else if (input == "1"){
+            std::cout << createPokemonList;
+        }
+        else if (input == "2"){
+            std::cout <<"Would you like to search by name or pokedex? \n ->";
+            std::string searchInput = "";
+            getline(std::cin, searchInput);
+            if (searchInput == "name") {
+                std::cout <<"What name? \n ->";
+                std::string nameInput = "";
+                getline(std::cin, nameInput);
+                pokeList ->find(nameInput);
+            }
+            else if (searchInput == "pokedex"){
+                std::cout <<"What pokedex number? \n ->";
+                std::string dexInput = "";
+                getline(std::cin, dexInput);
+                pokeList ->find(dexInput);
+            }
+            else {
+                std::cout << " TODO put a catch here?";
+            }
+        }
+        else if (input == "3"){
+            std::cout <<" type display in development";
+        }
+        else if (input == "4"){
+            std::cout <<" team display in development";
+        }
+        else if (input == "5"){
+            std::cout <<" type effectiveness display in development";
+        }
+        else if (input == "6"){
+            std::cout <<" create team in development";
+        }
+        else if (input == "7"){
+            std::cout <<" edit team in development";
+        }
+        else if (input == "8"){
+            std::cout <<" balanced team in development";
+        }
+        else if (input == "9"){
+            std::cout <<" fill team in development";
+        }
+        else if (input == "10"){
+            std::cout <<" counter team in development";
+        }
+        else if (input == "11"){
+            std::cout <<" import in development";
+        }
+        else if (input == "12"){
+            std::cout <<" export in development";
+        }
+
+        while(input2 > 12 || input2 < 1|| input != "quit"||input!="help"){
+            getline(std::cin, input);
+            try {
+                input2 = stoi(input);
+            }
+            catch(std::exception& e){
+                std::cout<<e.what()<<std::endl;
+            }
+        }
+
+    }
+}
